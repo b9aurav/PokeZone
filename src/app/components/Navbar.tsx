f6d1c32a8c125204aef6cbe22b5458bbb9cc71a4
Gaurav <hey9aurav@gmail.com>
2023-12-24 17:54:29 +0530

    Dark Mode button added.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
+import Image from "next/image";
+import ThemeToggleButton from "./ThemeToggleButton";
 
 type Props = {
   onSearchChange: (searchInput: string) => void;
@@ -12,15 +14,18 @@ const Navbar = (props: Props) => {
   const types: any = useSelector((state: RootState) => state.poke.types) || [];
 
   return (
-    <div className="fixed variant-glass-primary border-b-4 border-red-500 top-0 w-full p-2 flex z-50 lg:px-80 sm:px-4">
-      <label className=" font-bold mx-4 m-2 text-primary-500">PokeZone</label>
+    <div className="fixed variant-glass-primary border-b-4 border-red-500 top-0 w-full justify-center items-center p-2 flex z-50 lg:px-80 sm:px-4">
+      <div className="flex justify-center items-center mr-2 lg:mr-10 w-36">
+      <Image src="/pokeball.svg" alt="pokeball" width={50} height={50} />
+      <label className=" font-bold text-black hidden lg:block dark:text-[white]">PokeZone</label>
+      </div>
       <input
         className="input"
         type="text"
         placeholder="Search"
         onChange={(e) => props.onSearchChange(e.target.value)}
       />
-      <select className="select mx-4 rounded-full" name="filter" id="filter" onChange={e => props.onFilterChange(e.target.value)}>
+      <select className="select mx-4 rounded-[20px]" name="filter" id="filter" onChange={e => props.onFilterChange(e.target.value)}>
         <option value="">All</option>
         {types.map((type: any) => (
           <option value={type} key={type}>
@@ -28,6 +33,7 @@ const Navbar = (props: Props) => {
           </option>
         ))}
       </select>
+      <ThemeToggleButton onToggle={()=>{document.querySelector('html')?.classList.toggle('dark')}}/>
     </div>
   );
 };
